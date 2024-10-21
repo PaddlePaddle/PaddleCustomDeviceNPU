49d4d5449fb63ccdc644de57a29fb951ca7966bf
tianshuo78520a <707759223@qq.com>
2024-10-21 10:26:05 +0000

    Fix input

--- backends/npu/tools/pr_ci_npu.sh
+++ backends/npu/tools/pr_ci_npu.sh
@@ -191,7 +191,7 @@ function run_paddlex() {
     python main.py -c paddlex/configs/image_classification/ResNet50.yaml \
     -o Global.mode=predict \
     -o Predict.model_dir="./resnet50_output/best_model/inference" \
-    -o Predict.input_path="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_image_classification_001.jpg" \
+    -o Predict.input="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_image_classification_001.jpg" \
     -o Global.device="npu:${DEVICE}"
     echo "End PaddleX ResNet50"
 
@@ -205,7 +205,7 @@ function run_paddlex() {
     python main.py -c paddlex/configs/object_detection/PP-YOLOE_plus-S.yaml \
     -o Global.mode=predict \
     -o Predict.model_dir="./ppyolo_plus_s_output/best_model/inference" \
-    -o Predict.input_path="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_object_detection_002.png" \
+    -o Predict.input="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_object_detection_002.png" \
     -o Global.device="npu:${DEVICE}"
     echo "End PP-YOLOE+"
 
@@ -219,7 +219,7 @@ function run_paddlex() {
     python main.py -c paddlex/configs/semantic_segmentation/Deeplabv3_Plus-R50.yaml \
     -o Global.mode=predict \
     -o Predict.model_dir="./deeplabv3p_output/best_model/model/inference" \
-    -o Predict.input_path="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_semantic_segmentation_001.jpg" \
+    -o Predict.input="https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/general_semantic_segmentation_001.jpg" \
     -o Global.device="npu:${DEVICE}"
     echo "End DeepLabv3+"
 }
